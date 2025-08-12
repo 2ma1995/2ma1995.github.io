3bbfd26c9fd57993e328acb86c88a08e9ee6fe52
2ma1995 <vthquddbsv@gmail.com>
2025-08-12 15:10:10 +0900

    Deploy new blog post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754977448',resources: [
+    cacheName: 'chirpy-1754978969',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
