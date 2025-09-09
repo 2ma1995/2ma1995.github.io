fbd6b75bf38d0b9a16f2158944e5078016556ee8
2ma1995 <vthquddbsv@gmail.com>
2025-09-09 21:12:51 +0900

    🔄 자동 배포: 2025-09-09 21:12:51

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757419909',resources: [
+    cacheName: 'chirpy-1757419970',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
